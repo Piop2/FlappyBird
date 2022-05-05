f3f9fc75513442d029b53611390331f3428abf6e
Piop2 <seungyounyou0220@gmail.com>
2022-05-06 00:12:02 +0900

    add FadeIn

--- scripts/effect/vfx.py
+++ scripts/effect/vfx.py
@@ -1,6 +1,30 @@
 import pygame
 
 
+class FadeIn:
+    def __init__(self, color, speed):
+        self.color = color
+        self.alpha = 255
+        self.speed = speed
+
+    def init_alpha(self):
+        self.alpha = 255
+
+    def update(self, dt):
+        self.alpha -= self.speed * dt
+        if self.alpha <= 0:
+            self.alpha = 0
+            return True
+        return False
+
+    def render(self, surf):
+        fade_surf = pygame.Surface(surf.get_size())
+        fade_surf.fill(self.color)
+        fade_surf.set_alpha(self.alpha)
+
+        surf.blit(fade_surf, (0, 0))
+
+
 class FadeOut:
     def __init__(self, color, speed):
         self.color = color
@@ -16,12 +40,10 @@ class FadeOut:
             self.alpha = 255
             return True
         return False
-    
+
     def render(self, surf):
         fade_surf = pygame.Surface(surf.get_size())
         fade_surf.fill(self.color)
         fade_surf.set_alpha(self.alpha)
 
         surf.blit(fade_surf, (0, 0))
-    
-    
